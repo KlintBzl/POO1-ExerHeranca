9524bca1e050b40547ef7e30506ce326a64782b4
KlintBzl <klintburzlaff@gmail.com>
2025-03-05 20:41:47 -0300

    Exercicio 2 pronto

---
+++ Exerc2/src/exerc2/Exerc2.java
@@ -0,0 +1,11 @@
+package exerc2;
+
+public class Exerc2 {
+
+    public static void main(String[] args) {
+        Calculadora calculo1 = new Calculadora();
+        System.out.println("Resultado: " + calculo1.multiplicar(7.4, 5.1));
+        System.out.println("Resultado: " + calculo1.multiplicar(3, 3));
+        System.out.println("Resultado: " + calculo1.multiplicar(1, 2, 3));
+    }
+}
